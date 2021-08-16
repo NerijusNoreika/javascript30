d79bbb32452db28176fa4c8fc580bff462642dd3
NerijusNoreika <Akimbas123@gmail.com>
2021-08-16 14:55:45 +0300

    First day

---
+++ 01 - JavaScript Drum Kit/solution.js
@@ -0,0 +1,17 @@
+let keys = document.querySelectorAll('.key');
+
+window.addEventListener('keydown', function(e) {
+    let ascii = e.keyCode;
+    let elm = document.querySelector(`.key[data-key="${ascii}"]`)
+    if (elm) {
+        elm.classList.add('playing');
+        let audio = document.querySelector(`audio[data-key="${ascii}"]`)
+        if (audio) audio.play();
+        
+        this.setTimeout(() => {
+            elm.classList.remove('playing');
+        }, 200)
+    }
+});
+
+
---
+++ readme.md
@@ -0,0 +1,3 @@
+# JavaScript Wes Bos 30 day challenge (just for fun)
+
+Will be doing 30 JavaScript projects provided by Wes Bos just for fun
