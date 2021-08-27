be414e6fcdf4a16c7bf687f455801ea39bf0bcea
NerijusNoreika <Akimbas123@gmail.com>
2021-08-27 10:09:10 +0300

    12 challenge

---
+++ 12 - Key Sequence Detection/solution.js
@@ -0,0 +1,17 @@
+const shortcutTracking = [];
+const pattern = "ArrowUpArrowUpArrowDownArrowDownArrowLeftArrowRightArrowLeftArrowRightKeyBKeyAEnter";
+const len = 11;
+window.addEventListener('keydown', e => {
+    shortcutTracking.push(e.code);
+    if (shortcutTracking.length > len) {
+        shortcutTracking.splice(1);
+    }    
+    if (shortcutTracking.length === 11) {
+        let data = shortcutTracking.join('');
+        if (data === pattern) {
+            document.body.style.background = "#39f";
+            document.body.classList.add('pattern');
+            document.body.innerHTML = "<h1> Pattern triggered </h1>";
+        }
+    }
+});
