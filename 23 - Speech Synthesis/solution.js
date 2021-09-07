e1d92e627a1dea855a769874ee09c46dc618860f
NerijusNoreika <Akimbas123@gmail.com>
2021-09-07 21:54:28 +0300

    23 challenge

---
+++ 23 - Speech Synthesis/solution.js
@@ -0,0 +1,47 @@
+const voiceProps = new SpeechSynthesisUtterance();
+const synth = window.speechSynthesis;
+const voicesDropdown = document.querySelector('[name="voice"]');
+const options = document.querySelectorAll('[type="range"], [name="text"]');
+const speakButton = document.querySelector('#speak');
+const stopButton = document.querySelector('#stop');
+let voices = [];
+
+function populateVoices() {
+  voices = voices = synth.getVoices();
+  voicesDropdown.innerHTML = '';
+
+  for (let voiceKey in voices) {
+    voicesDropdown.innerHTML += `<option value="${voiceKey}">${voices[voiceKey].name}</option>`;
+  }
+}
+
+function setOption() {
+  voiceProps[this.name] = this.value;
+  reset(true);
+}
+
+function reset(cancel = true) {
+  synth.cancel();
+  if (cancel) {
+    synth.speak(voiceProps);
+  }
+}
+
+voicesDropdown.addEventListener('input', e => {
+  voiceProps.voice = voices[e.target.value];
+  reset(true);
+});
+
+speakButton.addEventListener('click', e => {
+  voiceProps.text = document.querySelector('[name="text"]').value;
+
+  synth.paused ? synth.resume() : synth.speak(voiceProps);
+});
+
+options.forEach(option => option.addEventListener('change', setOption));
+
+synth.addEventListener('voiceschanged', populateVoices);
+
+stopButton.addEventListener('click', e => {
+  synth.pause();
+});
