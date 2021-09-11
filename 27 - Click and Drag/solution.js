2c94b3caed8797d774d67f081cacac0594b9b3af
Nerijus Noreika <ner.noreika@gmail.com>
2021-09-11 21:16:12 +0300

    Challenge 27

---
+++ 27 - Click and Drag/solution.js
@@ -0,0 +1,41 @@
+let items = document.querySelector('.items');
+
+let mousePressedCoords = 0;
+let mouseReleasedCoords;
+let lastDraggedCoords;
+let mouseIsDown = false;
+let totalScroll = 0;
+
+function handleMouseDown(event) {
+    mouseIsDown = true;
+    mousePressedCoords = event.clientX;
+    items.classList.add('active');
+}
+
+function calculateScroll(pos) {
+   return totalScroll + mousePressedCoords - pos;
+}
+
+function handleMouseMove(event) {
+    if (!mouseIsDown) return; 
+
+    let pixelsToScroll = calculateScroll(event.clientX);
+    items.scroll(pixelsToScroll, 0, {behavior: 'smooth'});
+}   
+
+function handleMouseUp(event) {
+    mouseIsDown = false;
+    totalScroll += mousePressedCoords - event.clientX;
+    if (totalScroll < 0) {
+        totalScroll = 0;
+    } else if (totalScroll > items.scrollWidth - items.clientWidth) {
+        totalScroll = items.scrollWidth - items.clientWidth;
+    } 
+    console.log(totalScroll);
+    items.classList.remove('active');
+}
+
+
+items.addEventListener('mousedown', handleMouseDown);
+items.addEventListener('mouseup', handleMouseUp);
+items.addEventListener('mousemove', handleMouseMove);
